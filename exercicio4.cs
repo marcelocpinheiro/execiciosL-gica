b560946a1c9741ebb678b44c49e3efe5bf3efa52
Renan Rosa <renanrosapsi@yahoo.com>
2021-10-21 14:15:27 -0300

    fix ex4

--- exercicio4.cs
+++ exercicio4.cs
@@ -1,35 +1,39 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
 	{
 		int numero1 = gerarNumeroRandomico(100);
 		int numero2 = gerarNumeroRandomico(100);
-		
-		String[] operacoes = {"+", "-", "/", "*"};
-		
-		string operacaoSelecionada = operacoes[gerarNumeroRandomico(4)];
-		
-		Console.WriteLine("" + numero1, numero2, operacaoSelecionada);
-		
+
+		String[] operacoes = { "+", "-", "/", "*" };
+
+		String operacaoSelecionada = operacoes[gerarNumeroRandomico(4)];
+
 		Double resultado = calcular(numero1, numero2, operacaoSelecionada);
+
 		Console.WriteLine("Qual é o resultado de: " + numero1 + " " + operacaoSelecionada + " " + numero2 + " ?");
-		string respostaUsuario = Console.ReadLine();
+		String respostaUsuario = Console.ReadLine();
+
 		Double doubleRespostaUsuario = Double.Parse(respostaUsuario);
-		
-		Console.WriteLine("" + numero1, numero2, operacaoSelecionada);
-		if(doubleRespostaUsuario.Equals(resultado)) {
+
+		if (doubleRespostaUsuario.Equals(resultado))
+		{
 			Console.WriteLine("Parabéns, você acertou");
-		} else {
+		}
+		else
+		{
 			Console.WriteLine("Vá estudar " + nomeOperacao(operacaoSelecionada));
 		}
-		
-		
+
+
 	}
-	
-	public static Double calcular(int v1, int v2, string operacao) {
-		switch(operacao){
+
+	public static Double calcular(int v1, int v2, string operacao)
+	{
+		switch (operacao)
+		{
 			case "+":
 				return v1 + v2;
 			case "-":
@@ -42,9 +46,11 @@ public class Program
 				return 0;
 		}
 	}
-	
-	public static String nomeOperacao(string operacao) {
-		switch(operacao){
+
+	public static String nomeOperacao(string operacao)
+	{
+		switch (operacao)
+		{
 			case "+":
 				return "Soma";
 			case "-":
@@ -57,8 +63,9 @@ public class Program
 				return "Não encontrado";
 		}
 	}
-	
-	public static int gerarNumeroRandomico(int max) {
+
+	public static int gerarNumeroRandomico(int max)
+	{
 		Random random = new Random();
 		return random.Next(0, max);
 	}
