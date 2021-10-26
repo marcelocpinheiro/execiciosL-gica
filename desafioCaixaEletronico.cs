901b670ecfc4668cade123829c6b858375711a86
Marcelo Pinheiro <marcelompinheiro@outlook.com>
2021-10-26 20:13:06 +0100

    Desafio caixa eletronico

---
+++ desafioCaixaEletronico.cs
@@ -0,0 +1,35 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Olá Marcelo, seja bem vindo ao caixa do banco ahsefuihasefui
+
+        //1 - Consultar Saldo
+        //2 - Sacar
+        //3 - Depositar
+        //4 - Extrato
+        //5 - Sair
+
+        //1 - Consultar saldo
+        // saldo: R$ 0,00 no inicio
+        //Sempre mostra o saldo corrente da minha conta
+
+        //2 - Sacar
+        //Perguntar valor do saque
+        //Só posso sacar dinheiro que eu tenho
+
+        //3 - Depositar
+        //Perguntar valor que quero depositar
+        //Acrescenta o valor no saldo
+
+        //4 - Extrato
+        //Depositou 100
+        //Sacou 20
+        //Sacou 30
+        //Saldo Atual: R$ 50,00
+
+        //5 - Finalizar o programa
+	}
+}
